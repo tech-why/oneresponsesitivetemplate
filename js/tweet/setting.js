321bc56d90cd394bc48369d2b28966ee8afa3c93
why@111454 <1114545339@qq.com>
2014-11-24 21:25:06 +0800

    sheji
    Signed-off-by: why@111454 <1114545339@qq.com>

---
+++ js/tweet/setting.js
@@ -0,0 +1,15 @@
+						$(document).ready(function(){
+						   //TWITTER
+							$(".twitter").tweet({
+								  join_text: "auto",
+								  username: "envato",
+									count: 1,
+									auto_join_text_default: "we said,",
+									auto_join_text_ed: "we",
+									auto_join_text_ing: "we were",
+									auto_join_text_reply: "we replied",
+									auto_join_text_url: "we were checking out",
+									loading_text: "loading tweets..."
+							});
+						});
+						
